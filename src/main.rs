03c2032c4714ced4dccb6c4f88e97d12d85d5d08
Nathaniel Graham <[ngraham@redhat.com, nathaniel.graham@protonmail.com]>
2023-11-14 21:07:09 -0700

    maps and gw data

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,16 @@ fn main() {
         std::process::exit(1);
     }
 }
+
+// use mumblelink_reader::mumble_link_handler::MumbleLinkHandler;
+// use mumblelink_reader::mumble_link::MumbleLinkReader;
+// use std::{thread, time};
+
+// fn main() {
+//     let handler = MumbleLinkHandler::new().unwrap();
+//     loop {
+//         let linked_memory = handler.read().unwrap();
+//         println!("{:?}", linked_memory);
+//         thread::sleep(time::Duration::from_millis(50));
+//     }
+// }
