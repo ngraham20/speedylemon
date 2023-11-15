ded0e6a43e37e6e85b79782c5a2549bae6152d10
Nathaniel Graham <[ngraham@redhat.com, nathaniel.graham@protonmail.com]>
2023-11-14 23:05:01 -0700

    checkpoint file can be read from and written to

--- src/checkpoint.rs
+++ src/checkpoint.rs
@@ -1,16 +1,53 @@
+use std::{fs::OpenOptions, io::Write};
+
+use serde::{Deserialize, Serialize};
+use anyhow::{Result, anyhow};
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Checkpoint {
+    #[serde(rename = "STEP")]
     step: i16,
+    #[serde(rename = "STEPNAME")]
     stepname: String,
-    x: f64,
-    y: f64,
-    z: f64,
+    #[serde(rename = "X")]
+    x: f32,
+    #[serde(rename = "Y")]
+    y: f32,
+    #[serde(rename = "Z")]
+    z: f32,
+    #[serde(rename = "RADIUS")]
+    radius:Option<i32>
 }
 
-pub fn load_checkpoint_file() {
-    todo!()
+pub fn load_checkpoint_file(file: String) -> Result<Vec<Checkpoint>> {
+    let mut reader = csv::Reader::from_path(file)?;
+    let iter = reader.deserialize();
+    let mut checkpoints = Vec::new();
+
+    for record in iter {
+        let checkpoint = record?;
+        println!("{:?}", checkpoint);
+        checkpoints.push(checkpoint);
+    }
+
+    Ok(checkpoints)
+}
+
+pub fn write_checkpoints_to_file(checkpoints: Vec<Checkpoint>, path: String) -> Result<()> {
+    use std::fs::File;
+
+    let mut writer = csv::Writer::from_writer(vec![]);
+    for checkpoint in checkpoints.iter() {
+        writer.serialize(checkpoint)?;
+    }
+
+    let mut file = File::create(path)?;
+    file.write_all(&writer.into_inner()?)?;
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    
 }
--- src/speedylemon.rs
+++ src/speedylemon.rs
@@ -1,11 +1,14 @@
 use anyhow::Result;
 use super::guild_wars_handler;
+use super::checkpoint;
 
 pub fn run() -> Result<()> {
     // load checkpoint file from command argument or tui
     // get live mumble api data
     // as a base level functionality, output to the console when the next checkpoint is reached
 
-    guild_wars_handler::read_mumble()?;
+    // guild_wars_handler::read_mumble()?;
+    let checkpoints = checkpoint::load_checkpoint_file(String::from("maps/TYRIACUP/TYRIA DIESSA PLATEAU.csv"))?;
+    checkpoint::write_checkpoints_to_file(checkpoints, String::from("CHECKPOINT.csv"));
     Ok(())
 }
