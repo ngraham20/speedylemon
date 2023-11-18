2d3cbb8c36d4c037f75ea8b89e5bed37a8038f2b
Nathaniel Graham <[ngraham@redhat.com, nathaniel.graham@protonmail.com]>
2023-11-18 14:27:46 -0700

    added basic livesplit functionality

--- src/speedylemon.rs
+++ src/speedylemon.rs
@@ -1,10 +1,11 @@
 use anyhow::{Result, Context};
-use crate::course::CourseState;
+use crate::{course::CourseState, util::euclidian_distance};
 
 use super::guild_wars_handler;
 use super::course::Course;
 use log;
-use device_query::{DeviceQuery, DeviceState, Keycode, DeviceEvents};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use enigo::*;
 
 #[derive(PartialEq)]
 pub enum ProgramState {
@@ -45,6 +46,11 @@ pub fn run() -> Result<()> {
     // update the racer position
     // check if the position is inside the next checkpoint
 
+
+    // state can be maintained with multiple structs, each of which simply has `.update()` called on it.
+    // the struct's internal state can be handled from within.
+    // this state may be exported if sending messages between structs is necessary.
+
     let mut data = guild_wars_handler::GW2Data::new()?;
     data.init()?;
     data.update().context(format!("Failed to update GW2 Data"))?;
@@ -54,13 +60,30 @@ pub fn run() -> Result<()> {
 
     let mut state = ProgramState::Continue;
     let mut course_state = CourseState::WaitingToStart;
+    let mut enigo = Enigo::new();
     while state != ProgramState::Quit {
         data.update().context(format!("Failed to update GW2 Data"))?;
         let next_checkpoint = course.peek_next();
         let dst = super::util::euclidian_distance(data.racer.position, next_checkpoint.point());
+        if let Some(reset) = &course.reset {
+            let restart_dst = euclidian_distance(data.racer.position, reset.point());
+            if restart_dst < reset.radius as f32 {
+                course.restart();
+                enigo.key_down(Key::K);
+                std::thread::sleep(std::time::Duration::from_millis(50));
+                enigo.key_up(Key::K);
+    
+            }
+        }
         if dst < next_checkpoint.radius as f32 {
             course_state = course.collect_checkpoint();
+            // TODO: make a Livesplit struct with `split` and `reset` keys, then just call a function for split and reset
+            enigo.key_down(Key::L);
+            std::thread::sleep(std::time::Duration::from_millis(50));
+            enigo.key_up(Key::L);
         }
+
+        // TODO: Move course state functionality into a function
         if course_state == CourseState::WaitingToStart {
             log::debug!("Waiting for player to cross starting line")
         } else if course_state == CourseState::ApproachingFinishLine {
