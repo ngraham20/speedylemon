25851d7d6303cf439bf856d6427240526cfc1169
Nathaniel Graham <[ngraham@redhat.com, nathaniel.graham@protonmail.com]>
2023-11-17 18:09:45 -0700

    Checkpoints now read and write backwards-compatible with Killer's spedometer

--- src/course.rs
+++ src/course.rs
@@ -1,33 +1,60 @@
 use std::{fs::File, io::Write};
 
-use super::checkpoint::{Checkpoint, Stepname};
+use crate::checkpoint::Stepname;
+
+use super::checkpoint::Checkpoint;
 use anyhow::Result;
 use log;
 
 /// Course is a series of numbered checkpoints with dedicated Start, Reset, and End checkpoints
 pub struct Course {
     pub checkpoints: Vec<Checkpoint>,
+    pub reset: Option<Checkpoint>,
+    pub current_checkpoint: usize,
 }
 
 impl Course {
+
+    pub fn peek_next(&self) -> &Checkpoint {
+        &self.checkpoints[self.current_checkpoint]
+    }
+    pub fn collect_checkpoint(&mut self) {
+        if self.current_checkpoint < self.checkpoints.len() - 1 {
+            self.current_checkpoint += 1;
+        }
+    }
     pub fn from_path(path: String) -> Result<Course> {
         log::info!("Loading race course from path: {}", path);
         let mut reader = csv::Reader::from_path(path)?;
         let iter = reader.deserialize();
         let mut checkpoints = Vec::new();
+        let mut reset: Option<Checkpoint> = None;
     
         for record in iter {
             let checkpoint: Checkpoint = record?;
-            checkpoints.push(checkpoint);
+            match checkpoint.stepname {
+                Stepname::RESET => { reset = Some(checkpoint) },
+                Stepname::CHECKPOINT |
+                Stepname::START |
+                Stepname::END => { checkpoints.push(checkpoint) },
+            }
 
         }
+        checkpoints.sort_by(|a, b| a.step.partial_cmp(&b.step).unwrap());
         Ok(Course {
-            checkpoints: checkpoints
+            checkpoints: checkpoints,
+            reset: reset,
+            current_checkpoint: 0usize,
         })
     }
     
     pub fn export_to_path(&self, path: String) -> Result<()> {
         let mut writer = csv::Writer::from_writer(vec![]);
+
+        if let Some(cp) = &self.reset {
+            writer.serialize(cp)?;
+        }
+
         for checkpoint in self.checkpoints.iter() {
             writer.serialize(checkpoint)?;
         }
--- src/speedylemon.rs
+++ src/speedylemon.rs
@@ -2,8 +2,11 @@ use anyhow::{Result, Context};
 use super::guild_wars_handler;
 use super::course::Course;
 use log;
+use device_query::{DeviceQuery, DeviceState, Keycode};
 
 pub fn run() -> Result<()> {
+    let device_state = DeviceState::new();
+
     // load checkpoint file from command argument or tui
     // get live mumble api data
     // as a base level functionality, output to the console when the next checkpoint is reached
@@ -17,14 +20,22 @@ pub fn run() -> Result<()> {
     data.init()?;
     data.update().context(format!("Failed to update GW2 Data"))?;
     log::debug!("Name: {}, Racer Position: {:?}, Camera Position: {:?}", &data.racer.name, &data.racer.position, &data.camera.position);
-    let course = Course::from_path(String::from("maps/TYRIACUP/TYRIA DIESSA PLATEAU.csv"))?;
+    let mut course = Course::from_path(String::from("maps/TYRIACUP/TYRIA DIESSA PLATEAU.csv"))?;
     course.export_to_path(String::from("maps/RAEVENCUP/01-development.csv"))?;
 
-    loop {
+    let mut keys: Vec<Keycode> = Vec::new();
+    while !keys.contains(&Keycode::Q) {
         data.update().context(format!("Failed to update GW2 Data"))?;
-        let dst = super::util::euclidian_distance(data.racer.position, course.checkpoints[0].point());
-        log::debug!("Distance to first checkpoint: {}", dst);
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        let dst = super::util::euclidian_distance(data.racer.position, course.peek_next().point());
+        log::debug!("Distance to checkpoint {}: {}", course.current_checkpoint, dst);
+
+        keys = device_state.get_keys();
+        if keys.contains(&Keycode::N) {
+            course.collect_checkpoint();
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
+    log::info!("Terminating program.");
+    std::thread::sleep(std::time::Duration::from_secs(1));
     Ok(())
 }
