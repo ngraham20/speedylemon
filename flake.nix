a173f0e9d13e45a35c6b2cd971fed006cd0bcac5
Nathaniel Graham <nathaniel.graham@protonmail.com>
2023-12-06 21:58:08 -0700

    flake.nix is functional, but not ideal

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            pkgs.xorg.libX11.dev
+            pkgs.pkg-config
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
