34f08173b56cea349c96696d80b97edfaee8ada7
Nathaniel Graham <nathaniel.graham@protonmail.com>
2023-12-21 21:20:25 -0800

    can download checkpoint data now

---
+++ CONTRIBUTING.md
@@ -0,0 +1,18 @@
+# Beetlerank API
+## GET
+- MOTD: `https://www.beetlerank.com/api/info`
+- Cups: `https://www.beetlerank.com/api/cups`
+- Maps: `https://www.beetlerank.com/api/maps/<name CUP>`
+- Rank: `https://www.beetlerank.com/rank/api/<course>/<username>`
+
+## POST
+`https://www.beetlerank.com/upload-log`
+- `user: <username>`
+- `guildhall: <course>`
+- `file: <logfile>`
+
+# Debugging
+Logging is handled with the environment variable `RUST_LOG`. Set it to **error**, **info**, **debug**, or **trace** for each desired log level.
+
+Logging for release builds is disabled, so the while during debugging, you may see log messages clutter your console application, these will 
+not be present in the release build. You can see this for yourself by running `cargo run --release`
--- src/course.rs
+++ src/course.rs
@@ -1,13 +1,12 @@
 use std::{fs::File, io::Write};
 
 use crate::checkpoint::Stepname;
-
 use crate::checkpoint::Checkpoint;
 use anyhow::Result;
 use log;
 
 /// Course is a series of numbered checkpoints with dedicated Start, Reset, and End checkpoints
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Course {
     pub name: String,
     pub checkpoints: Vec<Checkpoint>,
@@ -21,6 +20,35 @@ impl Course {
             checkpoints: Vec::new(),
             reset: None,
         }
+    }
+
+    pub fn from_url(url: String) -> Result<Course> {
+        log::info!("Loading race course from url: {}", url);
+        let path = url.clone();
+        let course_name = std::path::Path::new(&path).file_stem().unwrap().to_str().unwrap();
+        
+        let response = reqwest::blocking::get(url)?.text()?;
+        let mut reader = csv::Reader::from_reader(response.as_bytes());
+        let iter = reader.deserialize();
+        let mut checkpoints = Vec::new();
+        let mut reset: Option<Checkpoint> = None;
+
+        for record in iter {
+            let checkpoint: Checkpoint = record?;
+            match checkpoint.stepname {
+                Stepname::Reset => { reset = Some(checkpoint) },
+                Stepname::Checkpoint |
+                Stepname::Start |
+                Stepname::End => { checkpoints.push(checkpoint) },
+            }
+
+        }
+        checkpoints.sort_by(|a, b| a.step.partial_cmp(&b.step).unwrap());
+        Ok(Course {
+            name: String::from(course_name),
+            checkpoints: checkpoints,
+            reset: reset,
+        })
     }
 
     pub fn from_path(path: String) -> Result<Course> {
@@ -64,5 +92,19 @@ impl Course {
         let mut file = File::create(path)?;
         file.write_all(&writer.into_inner()?)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Context;
+
+    use super::*;
+
+    #[test]
+    fn test_course_from_url() {
+        let url = String::from("https://www.beetlerank.com/uploads/checkpoints/TYRIA GENDARRAN.csv");
+        let course = Course::from_url(url.clone()).context(format!("Failed to load checkpoint file from url: {}", url)).unwrap();
+        assert!(course.checkpoints.len() == 14)
     }
 }
