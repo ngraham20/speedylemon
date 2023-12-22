a02a6f3e764bb9964d43e194e55dcec487aa5a6c
Nathaniel Graham <nathaniel.graham@protonmail.com>
2023-12-22 14:30:48 -0800

    moved beetlerank local test server into this project

---
+++ beetlerank/index.js
@@ -0,0 +1,11 @@
+// const http = require('http');
+var express = require('express');
+const app = express();
+const hostname = 'localhost';
+const port = 3000;
+
+const devapi = require('./api')
+
+app.use('/api/dev', devapi);
+
+app.listen(port, hostname);
