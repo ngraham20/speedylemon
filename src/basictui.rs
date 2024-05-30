cb8b9338cbd8d0bca554a2b4c4881db4f5d89f20
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-05-30 17:22:00 -0600

    updated some UI flickering

--- src/basictui.rs
+++ src/basictui.rs
@@ -47,29 +47,31 @@ pub fn blit(ctx: &mut LemonContext) {
     // clear the screen
     print!("{esc}[2J{esc}[1;1H", esc=27 as char);
 
-    println!("Track: {}", ctx.course.name);
+    let mut window = String::from(format!("Track: {}\n", ctx.course.name));
+
     match ctx.race_state {
         RaceState::Finished => {
-            println!("Race Finished!");
-            println!("Lap Time: {}", 6);
+            window.push_str("Race Finished!\n");
+            window.push_str(&format!("Lap Time: {:?}", ctx.checkpoint_times.last().unwrap().timestamp()));
         },
         _ => {
             
-            println!("Checkpoint: {}", ctx.current_checkpoint);
-            println!("Distance to next checkpoint: {}", if ctx.current_checkpoint < ctx.course.checkpoints.len() {
+            window.push_str(&format!("Checkpoint: {}\n", ctx.current_checkpoint));
+            window.push_str(&format!("Distance to next checkpoint: {}\n", if ctx.current_checkpoint < ctx.course.checkpoints.len() {
                 ctx.current_cp_distance()} else {
                     -1.0
-                });
-            println!("Distance to reset checkpoint: {}", ctx.reset_cp_distance().unwrap_or(-1.0));
-            println!("Speed: {:?}", ctx.filtered_speed());
-            println!("----- Checkpoint Times -----");
+                }));
+            window.push_str(&format!("Distance to reset checkpoint: {}\n", ctx.reset_cp_distance().unwrap_or(-1.0)));
+            window.push_str(&format!("Speed: {:?}\n", ctx.filtered_speed()));
+            window.push_str("----- Checkpoint Times -----\n");
             for (idx, dur) in ctx.checkpoint_times.iter().enumerate() {
-                println!("Checkpoint: {}, Time: {}, Delta: {}", idx, dur.timestamp(), match idx {
+                window.push_str(&format!("Checkpoint: {}, Time: {}, Delta: {}\n", idx, dur.timestamp(), match idx {
                     0 => dur.timestamp(),
                     _ => dur.saturating_sub(ctx.checkpoint_times[idx-1]).timestamp()
-                })
+                }))
             }
         }
     }
+    print!("{}", window);
     
 }
