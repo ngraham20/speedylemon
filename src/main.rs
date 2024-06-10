63b056d43a173abcf490bb2e9d1e492f71dd5dcb
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-06-10 00:50:23 -0600

    begining to add gui

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-mod track_selector;
-mod speedylemon;
-mod speedometer;
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-use pretty_env_logger;
-use log;
-use std::cell::Cell;
-
-thread_local!(static DEBUG: Cell<bool> = Cell::new(false));
-
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-
-    if let Err(err) = speedylemon::run() {
-        log::error!("Error: {:?}", err);
-        std::process::exit(1);
-    }
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(speedylemon::TemplateApp::new(cc))),
+    )
 }
