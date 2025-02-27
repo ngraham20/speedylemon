cf115d610d98de4417d711d1e0aceff70f9a3e99
Nathaniel Graham <nathaniel.graham@protonmail.com>
2025-02-27 00:57:00 -0700

    updated justfile with distrobox commands

--- justfile
+++ justfile
@@ -1,2 +1,17 @@
 test:
 	scripts/cargotest.sh
+
+build *release:
+	CROSS_CONTAINER_ENGINE=podman cross build --target=x86_64-pc-windows-gnu {{release}}
+
+dbox-debug:
+	distrobox-host-exec ./run.sh --debug
+
+debug:
+	./run.sh --debug
+
+dbox-run:
+	distrobox-host-exec ./run.sh --release
+
+run:
+	./run.sh --release
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USER=$(whoami)
-PROTON="Proton 8.0"
-PROTON_PATH="/home/$USER/.local/share/Steam/steamapps/common/$PROTON/dist/bin/wine"
+PROTON_PATH="/home/$USER/.local/share/Steam/compatibilitytools.d/GE-Proton9-23/files/bin/wine"
 GUILD_WARS_PATH="/home/$USER/.local/share/Steam/steamapps/common/Guild Wars 2"
 GUILD_WARS_PREFIX_PATH="/home/$USER/.local/share/Steam/steamapps/compatdata/1284210/pfx"
+
 # Environment variables
 export DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1="1"
 export __GL_SHADER_DISK_CACHE="1"
@@ -20,8 +20,19 @@ export DXVK_NVAPIHACK="0"
 export DXVK_ENABLE_NVAPI="1"
 export WINEDLLOVERRIDES="winemenubuilder="
 export WINE_LARGE_ADDRESS_AWARE="1"
-#export TERM="xterm"
 
-# Command
-gamemoderun "$PROTON_PATH" "target/x86_64-pc-windows-gnu/release/speedylemon.exe"
 
+debug() {
+    "$PROTON_PATH" "target/x86_64-pc-windows-gnu/debug/speedylemon.exe"
+}
+
+release() {
+    "$PROTON_PATH" "target/x86_64-pc-windows-gnu/release/speedylemon.exe"
+}
+
+case $1 in
+    "--debug")
+    debug;;
+    "--release")
+    release;;
+esac
